89b91a8be7f1c4f60a3d1b33fb2ef4474ef6080a
ChristopherVuvu <vuvumumo93@gmail.com>
2023-07-25 23:02:55 +0300

    summarized the routes for land to only the CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :vehicles
+  resources :lands, only: [:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index" # GET /lands
-  # Get all lands
-  get '/lands', to: 'lands#index'
-
-  # GET /lands/:id
-  # Get a specific land by ID
-  get '/lands/:id', to: 'lands#show'
-
-  # POST /lands
-  # Create a new land entry
-  post '/lands', to: 'lands#create'
-
-  # PATCH/PUT /lands/:id
-  # Update a specific land by ID
-  put '/lands/:id', to: 'lands#update'
-
-  # DELETE /lands/:id
-  # Delete a specific land by ID
-  delete '/lands/:id', to: 'lands#destroy'
 end

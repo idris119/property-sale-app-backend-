3e2d287ae39f4cd99ba6cac7d1a799697079501d
ChristopherVuvu <vuvumumo93@gmail.com>
2023-07-25 22:59:25 +0300

    added the lands routes and has Get, Post, Put and delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,23 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index" # GET /lands
+  # Get all lands
+  get '/lands', to: 'lands#index'
+
+  # GET /lands/:id
+  # Get a specific land by ID
+  get '/lands/:id', to: 'lands#show'
+
+  # POST /lands
+  # Create a new land entry
+  post '/lands', to: 'lands#create'
+
+  # PATCH/PUT /lands/:id
+  # Update a specific land by ID
+  put '/lands/:id', to: 'lands#update'
+
+  # DELETE /lands/:id
+  # Delete a specific land by ID
+  delete '/lands/:id', to: 'lands#destroy'
 end

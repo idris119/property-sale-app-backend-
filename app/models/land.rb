213689b205304a7a81d5266011c628df5d76be67
Noah <noahmwangi@gmail.com>
2023-07-26 11:42:25 +0300

    Added featured column to Houses, Lands, and vehicles

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,2 +1,9 @@
 class Land < ApplicationRecord
+    def up
+      add_column :lands, :featured, :boolean, default: false
+    end
+  
+    def down
+      remove_column :lands, :featured
+    end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,10 @@
 class Vehicle < ApplicationRecord
     belongs_to :user
+    def up
+      add_column :vehicles, :featured, :boolean, default: false
+    end
+  
+    def down
+      remove_column :vehicles, :featured
+    end
 end

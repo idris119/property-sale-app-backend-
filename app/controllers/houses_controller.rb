b7077bc9cc962444192438cc359fb04da12fef6d
Noah <noahmwangi@gmail.com>
2023-07-26 11:08:06 +0300

    search by location and filter by price range

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,57 +1,66 @@
 class HousesController < ApplicationController
-       #used to hinder repetition of code 
-       before_action :set_house, only: [:show, :edit, :update, :destroy]
-  
-       def index
-         house = House.all
-         render json: house
-       end
-     
-       def show
-        house = House.find_by(id: params[:id])
-        render json: house
-       end
-     
-     
-       def create
-   
-         House = House.create(house_params)
-           if House.save
-               render json: House.to_json(except: [:created_at, :updated_at]), status: :created
-             else
-               render json: { error: "Failed to create House" }, status: :unprocessable_entity
-             end   
-            end
-     
-     
-       def update
-           House = find_House
-           if House.update(house_params)
-             render json: House.to_json(except: [:created_at, :updated_at])
-           else
-             render json: { error: "Failed to update House" }, status: :unprocessable_entity
-           end
-         
-       end
-     
-       def destroy
-           House = find_House
-           if House.destroy
-             head :no_content, status: :no_content
-           else
-             render json: { error: "Failed to delete House" }, status: :unprocessable_entity
-           end
-        
-       end
-     
-       private
-     
-       def set_house
-         House.find_by(id :params[:id])
-       end
-     
-       def house_params
-         params.require(:house).permit(:type, :bedrooms, :price, :distance, :description)
-       end
-     end
+  before_action :set_house, only: [:show, :edit, :update, :destroy]
+
+  def index
+    houses = House.all
+    render json: houses
+  end
+
+  def show
+    render json: @house
+  end
+
+  def create
+    house = House.new(house_params)
+    if house.save
+      render json: house.to_json(except: [:created_at, :updated_at]), status: :created
+    else
+      render json: { error: "Failed to create House" }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @house.update(house_params)
+      render json: @house.to_json(except: [:created_at, :updated_at])
+    else
+      render json: { error: "Failed to update House" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @house.destroy
+      head :no_content, status: :no_content
+    else
+      render json: { error: "Failed to delete House" }, status: :unprocessable_entity
+    end
+  end
+
+  # Additional Actions
+
+  def search_by_location
+    houses = House.where("location LIKE ?", "%#{params[:location]}%")
+    render json: houses
+  end
+
+  def filter_by_price_range
+    min_price = params[:min_price].to_f
+    max_price = params[:max_price].to_f
+    houses = House.where(price: min_price..max_price)
+    render json: houses
+  end
+
+  def featured_houses
+    houses = House.where(featured: true)
+    render json: houses
+  end  
+
+  private
+
+  def set_house
+    @house = House.find_by(id: params[:id])
+  end
+
+  def house_params
+    params.require(:house).permit(:location, :size, :price, :bedrooms, :bathrooms, :amenities, :images)
+  end
 end
